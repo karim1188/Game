ac84ecde720febfbb2e2d3d0b47560d345594104
nayera99 <96743800+nayera99@users.noreply.github.com>
2021-12-30 11:22:52 +0200

    commit

---
+++ New Unity/Assets/scipts/playercontroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+  
+
+    
+
+ public float moveSpeed;
+    public float jumpHeight;
+    public bool isFacingRight;
+    public KeyCode Spacebar;
+    public KeyCode L;
+    public KeyCode R;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool grounded;
+    private Animator anim;
+    void Start()
+    {
+
+        isFacingRight = true;
+        anim = GetComponent<Animator>();
+
+    }
+
+
+   
+        
+}
+    
